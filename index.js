e8e7f2420a9261bce4d66cfd491eb21a7d1da7eb
NamHuynh <mooncomvn130@gmail.com>
2021-01-09 13:26:30 +0700

    login dashboard signup

--- index.js
+++ index.js
@@ -1,14 +1,61 @@
-
-
 var express = require("express"); //Nạp thư viện express
+var bodyParser = require("body-parser");
 var app = express(); //Gọi thư viện để sử dụng
 app.use(express.static("public")); //Mặc định thư mục ban đầu là public
 app.set("view engine", "ejs"); //Sử dụng ejs
 app.set("views", "./views"); //Thư mục views để chứa các ejs...
+const bcrypt = require('bcrypt');
+// const LocalStrategy = require("passport-local").Strategy;
+// function initialize(passport){
+//     const autheticateUser = (email, password, done)=>{
+//         pool.query(
+//             `SELECT * FROM users WHERE email = $1`,
+//             [email],
+//             (err,result)=>{
+//             if(err){
+//                 throw err;
+//             }
+//             console.log(result.rows);
+//             if(result.rows.length>0){
+//                 const user = result.rows[0];
+//                 bcrypt.compare(password, user.password,(err, isMatch)=>{
+//                     if(err){
+//                         throw err;
+//                     }
+//                     if(isMatch){
+//                         return done(null, user);
+//                     }else{
+//                         return done(null, false, {message:"Password is not correct"})
+//                     }
+//                 });
+//             }else{
+//                 return done(null, false, {message: "Email is not registered!"})
+//             }
+//         }
+//         )
+//     }
+//     passport.use(new LocalStrategy({
+//         usernameField: "email",
+//         passwordField: "password", 
+//     }, autheticateUser
+//     )
+//     );
+//     passport.serializeuser((user, done)=>done(null, user.id));
+//     passport.deserializeUser((id, done)=>{
+//         pool.query(
+//             `SELECT * FROM users WHERE id = $1`, [id], (err, result)=>{
+//                 if(err){
+//                     throw err;
+//                 }
+//                 return done(null, result.rows[0]);
+//             }
+//         );
+//     });
+// }
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-
-app.listen(3000); //Port  
+app.listen(3000);
 
 var pg = require("pg");
 var config = {
@@ -22,10 +69,12 @@ var config = {
 };
 
 
+
 var bodyParser = require('body-parser');
 var urlencodeParser = bodyParser.urlencoded({ extended:false });
 
 var multer  = require('multer');
+const { name } = require("ejs");
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, './public/upload')
@@ -69,13 +118,11 @@ app.get("/index.html", function(req,res)
             if(err){
                 return console.error('error running query', err);
             }
-            
             res.render("home.ejs",{data:result});
         });
     });
     
 })
-
 
 
 app.get("/shop.html", function(req,res){
@@ -267,9 +314,110 @@ app.get("/login.html", function(req,res){
     res.render("login");
 });
 
+app.post("/login.html",function(req,res){
+    var message=[];
+    pool.connect(function(err, client, done){
+        if(err){
+            return console.error('error fetching client from pool', err);
+        }
+        var sql = "select * from users where email= '"+req.body.email+"' ";
+        client.query(sql, function(err, result){
+            client.query("select * from users where password= '"+req.body.password+"' ", function(err, result1){
+                done();
+                if(err){
+                    return console.error('error running query', err);
+                }
+                if(result.rows.length<1){
+                    message = "Email does not exist!";
+                    res.render("login", {mess:message});
+                }
+                if(result1.rows.length<1){
+                    message = "Password is not correct!";
+                    res.render("login", {mess:message});
+                }
+                
+                else{
+                    var id = result.rows[0].id;
+                    res.redirect("user/dashboard/"+id+"");
+                // res.render("dashboard", {char:result1.rows[0]});
+                }
+            
+            });   
+            
+        });   
+    });
+});
+
+app.post("/register.html",async function(req,res){
+
+    var email = req.body.email;
+    var name = req.body.name;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+    var message =[];
+    // console.log(email);
+    if(password!=password2){
+        message ="Password do not match!";
+        res.render("register", {mess:message});
+    }
+    else{
+        // let hashedPassword = await bcrypt.hash(password, 10);
+        // console.log(hashedPassword);
+        pool.connect(function(err, client, done){
+            if(err){
+                return console.error('error fetching client from pool', err);
+            }
+            var sql = "select * from users where email= '"+req.body.email+"' ";
+            client.query(sql, function(err, result){
+                done();
+                if(err){
+                    return console.error('error running query', err);
+                }
+                if(result.rows.length>0){
+                     message = "Email already registered";
+                     res.render("register", {mess:message});
+                }
+                else{
+                    pool.connect(function(err, client, done){
+                        if(err){
+                            return console.error('error fetching client from pool', err);
+                        }
+                        client.query("insert into users (email, name, password) values ('"+req.body.email+"','"+req.body.name+"','"+req.body.password+"')", function(err, result){
+                            done();
+                            if(err){
+                                return console.error('error running query', err);
+                            }
+                            res.render("login");
+                        });
+
+                });
+                 }
+            });
+            
+        });
+
+    }
+        // pool.query(
+        //     `select * from user where email= $email`,
+        //     [email],
+        //     (err, result) => {
+        //          console.log(result.rows);
+        //          if(result.rows.length>0){
+        //              message = "Email already registered";
+        //              res.render("register", {mess:message});
+        //          }
+        //     }
+
+        // );
+
+
+});
+
 app.get("/register.html", function(req,res){
+    
     res.render("register");
 });
+
 
 app.get("/product/:id", function(req,res){
     var id = req.params.id;
@@ -308,39 +456,64 @@ app.get("/shop.html/:id", function(req,res){
     });
 });
 
+app.get("/user/dashboard/:id", function(req,res){
+    var id = req.params.id;
+    pool.connect(function(err, client, done){
+        if(err){
+            return console.error('error fetching client from pool', err);
+        }
+        client.query('select * from users where id =' + id, function(err, result){
+            done();
+            if(err){
+                return console.error('error running query', err);
+            }
+            res.render("dashboard",{char:result.rows[0]});
+        });
+        
+    });
+});
 
+app.post("/user/dashboard/:id",urlencodeParser, function(req,res){
+    var id=req.params.id;
+    upload(req, res, function (err) {
+        if (err) {
+            res.send("error");
+        }
+        else{
+            if(typeof(req.file)=='undefined'){
+                pool.connect(function(err, client, done){
+                    if(err){
+                        return console.error('error fetching client from pool', err);
+                    }
+                    var sql = "update users set name = '"+req.body.name+"', email = '"+req.body.email+"', phone = '"+req.body.phone+"' where id="+id;
+                    client.query(sql, function(err, result){
+                        done();
+                        if(err){
+                            return console.error('error running query', err);
+                        }
+                        res.redirect("../dashboard/"+id+"");
+                    });
+                    
+                });
+            }
+            else{
+                pool.connect(function(err, client, done){
+                    if(err){
+                        return console.error('error fetching client from pool', err);
+                    }
+                    var sql = "update users set name = '"+req.body.name+"', email = '"+req.body.email+"', phone = '"+req.body.phone+"', image= '"+req.file.originalname+"' where id="+id;
+                    client.query(sql, function(err, result){
+                        done();
+                        if(err){
+                            return console.error('error running query', err);
+                        }
+                        res.redirect("../dashboard/"+id+"");
+                    });
+                    
+                });
+            }
+            
+        }
+      });
+});
 
-
-// // -----------------------------LOGIN SIGN UP---------------------------
-
-// const sequelize = require('sequelize'); //Seqielize là thư viện thêm vào để tạo hệ thống đăng nhập, đăng ký
-// const db = new sequelize({
-//     database: 'book_shop',
-//     username: 'postgres',
-//     password: '0918303693',
-//     host: 'localhost',
-//     port: '5432',
-//     dialect: 'postgres',
-//     dialectOptions: {
-//         ssl: false
-//     },
-//     define: {
-//         freezeTableName: true
-//     },
-// });
-
-// db.authenticate()
-// .then(() => console.log('Ket noi thanh cong'))
-// .catch(err => console.log(err.message))
-
-// const user = db.define('user',{
-//     username: sequelize.STRING,
-//     password: sequelize.STRING,
-// })
-
-// db.sync()
-
-// user.bulkCreate({
-//     username: 'admin',
-//     password: 'admin1234'
-// }).then(user => console.log(user.get({plain: true})))
